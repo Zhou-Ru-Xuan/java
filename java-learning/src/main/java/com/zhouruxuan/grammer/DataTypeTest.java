33f8b0898e5f5f966b180a68c2517d61f5a837fb
zhouruxuan <zhouruxuan@meituan.com>
2023-10-08 19:16:59 +0800

    +=隐含强制类型转换

--- java-learning/src/main/java/com/zhouruxuan/grammer/DataTypeTest.java
+++ java-learning/src/main/java/com/zhouruxuan/grammer/DataTypeTest.java
@@ -7,4 +7,12 @@ public class DataTypeTest {
     public void testByteShort() {
         byte a = 1; //byte a = (byte) 1
     }
+
+    @Test
+    public void dataTypeTest() {
+        byte s1 = 127;
+        // s1 =  s1 + 1; //exception
+        s1 += 1;
+        System.out.println(s1); // -128
+    }
 }
