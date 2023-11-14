f49322f122334a63bed2b098ff1b3f0ac0129604
zhouruxuan <zhouruxuan@meituan.com>
2023-11-14 20:47:58 +0800

    spring depend on

---
+++ spring-learning/src/main/java/com/example/springlearning/bean/Config.java
@@ -0,0 +1,31 @@
+package com.example.springlearning.bean;
+
+import com.example.springlearning.entity.file.FileProcessor;
+import com.example.springlearning.entity.file.FileReader;
+import com.example.springlearning.entity.file.FileWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+
+@Configuration
+@ComponentScan("com.example.springlearning.entity")
+public class Config {
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    @DependsOn({"fileReader", "fileWriter"})
+    public FileProcessor fileProcessor() {
+        return new FileProcessor();
+    }
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    public FileReader fileReader() {
+        return new FileReader();
+    }
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    public FileWriter fileWriter() {
+        return new FileWriter();
+    }
+}
--- spring-learning/src/test/java/com/example/springlearning/SpringLearningApplicationTests.java
+++ spring-learning/src/test/java/com/example/springlearning/SpringLearningApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.springlearning;
 
 import com.example.springlearning.dao.PersonDao;
 import com.example.springlearning.entity.Person;
+import com.example.springlearning.entity.file.FileProcessor;
 import com.example.springlearning.service.StudentService;
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Slf4j
@@ -20,9 +24,17 @@ class SpringLearningApplicationTests {
     @Resource
     private PersonDao personDao;
 
+    @Resource
+    private FileProcessor fileProcessor;
+
     @Test
     void test() {
         Person personById = personDao.getPersonById(123L);
         LOGGER.info(personById.toString());
+    }
+
+    @Test
+    public void WhenFileProcessorIsCreated_FileTextContains_Processed() {
+        assertTrue(fileProcessor.process().endsWith("processed"));
     }
 }
