df27b01ac6bfb0aaf7857e0fb6c782c153670505
zhouruxuan <zhouruxuan@meituan.com>
2023-11-15 09:02:15 +0800

    spring depend on

--- spring-learning/src/test/java/com/example/springlearning/SpringLearningApplicationTests.java
+++ spring-learning/src/test/java/com/example/springlearning/SpringLearningApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.springlearning;
 
 import com.example.springlearning.dao.PersonDao;
 import com.example.springlearning.entity.Person;
-import com.example.springlearning.entity.file.FileProcessor;
 import com.example.springlearning.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -10,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Slf4j
@@ -24,17 +21,9 @@ class SpringLearningApplicationTests {
     @Resource
     private PersonDao personDao;
 
-    @Resource
-    private FileProcessor fileProcessor;
-
     @Test
     void test() {
         Person personById = personDao.getPersonById(123L);
         LOGGER.info(personById.toString());
-    }
-
-    @Test
-    public void WhenFileProcessorIsCreated_FileTextContains_Processed() {
-        assertTrue(fileProcessor.process().endsWith("processed"));
     }
 }
