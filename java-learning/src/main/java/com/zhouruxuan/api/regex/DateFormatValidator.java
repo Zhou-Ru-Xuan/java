0f132c0314770582a0783bc9f39845699165e251
zhouruxuan <zhouruxuan@meituan.com>
2023-12-01 17:14:18 +0800

    引入hibernate-validator

--- java-learning/src/main/java/com/zhouruxuan/api/regex/DateFormatValidator.java
+++ java-learning/src/main/java/com/zhouruxuan/api/regex/DateFormatValidator.java
@@ -3,6 +3,9 @@ package com.zhouruxuan.api.regex;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.Validation;
+import javax.validation.executable.ExecutableValidator;
+import java.lang.reflect.Method;
 import java.util.regex.Pattern;
 
 public class DateFormatValidator {
@@ -25,5 +28,16 @@ public class DateFormatValidator {
         Assertions.assertFalse(isValidDateFormat(dateString2));
         Assertions.assertTrue(isValidDateFormat(dateString3));
         Assertions.assertFalse(isValidDateFormat(dateString4));
+    }
+
+    @Test
+    public void testAnnotation() throws NoSuchMethodException {
+        testPattern("12345");
+    }
+
+    public void testPattern(@javax.validation.constraints.Pattern(regexp = DATE_FORMAT_REGEX, message = "格式错误") String date) throws NoSuchMethodException {
+        ExecutableValidator executableValidator = Validation.buildDefaultValidatorFactory().getValidator().forExecutables();
+        DateFormatValidator validator =  new DateFormatValidator();
+        Method method = DateFormatValidator.class.getMethod("testPattern", String.class);
     }
 }
