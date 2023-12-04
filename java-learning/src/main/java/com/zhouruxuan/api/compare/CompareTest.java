0ee7d45e5bef096ab7c6901e9193f78658888386
zhouruxuan <zhouruxuan@meituan.com>
2023-12-04 21:31:04 +0800

    根据给定的顺序排序

--- java-learning/src/main/java/com/zhouruxuan/api/compare/CompareTest.java
+++ java-learning/src/main/java/com/zhouruxuan/api/compare/CompareTest.java
@@ -5,9 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class CompareTest {
     @Test
@@ -21,6 +19,21 @@ public class CompareTest {
         list.sort(Comparator.comparing(CompareBean::getA,Comparator.nullsFirst(Boolean::compare)).reversed());
         System.out.println(list);
     }
+
+    @Test
+    public void testArrCmp(){
+        List<Integer> A = Arrays.asList(1, 3, 5, 7, 9);
+        List<CompareBean> B = Arrays.asList(new CompareBean(3), new CompareBean(9), new CompareBean(1), new CompareBean(7), new CompareBean(5));
+
+        // 使用自定义的Comparator进行排序
+        B.sort((o1, o2) -> {
+            int index1 = A.indexOf(o1.getNo());
+            int index2 = A.indexOf(o2.getNo());
+            return Integer.compare(index1, index2);
+        });
+
+        System.out.println(B); // 打印排序后的序列B
+    }
 }
 
 @NoArgsConstructor
@@ -28,4 +41,13 @@ public class CompareTest {
 @Data
 class CompareBean{
     Boolean a;
+    int no;
+
+    public CompareBean(Boolean a) {
+        this.a = a;
+    }
+
+    public CompareBean(int no) {
+        this.no = no;
+    }
 }
