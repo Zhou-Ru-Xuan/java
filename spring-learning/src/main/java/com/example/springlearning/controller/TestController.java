d050361ba16cb460b602ea3ed3238e28b0419f99
zhouruxuan <zhouruxuan@meituan.com>
2024-01-12 17:46:11 +0800

    验证切面失效的问题

--- spring-learning/src/main/java/com/example/springlearning/controller/TestController.java
+++ spring-learning/src/main/java/com/example/springlearning/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.example.springlearning.controller;
 
 
+import com.example.springlearning.annotation.ShowMethodName;
 import com.example.springlearning.entity.Person;
 import com.example.springlearning.result.ApiResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +17,8 @@ import java.util.Map;
 
 @RestController
 @Slf4j
-public class TestController {
+public class TestController implements ApplicationContextAware {
+    public ApplicationContext applicationContext;
 
     @PostMapping("/test")
     public void testMethod(@RequestBody Map<String, Object> requestBody) {
@@ -29,8 +34,20 @@ public class TestController {
     }
 
     @PostMapping("/testPerson")
+    @ShowMethodName
     public ApiResult<Boolean> testPerson(@RequestBody Person person) {
         LOGGER.info("person={} age={}", person.toString(), person.getAge());
+        applicationContext.getBean(TestController.class).updatePersonName();
+        // updatePersonName(); //切面失效，因为可以直接内部调用，不会经过代理
         return new ApiResult<>(200, null, true);
+    }
+
+    @ShowMethodName
+    public void updatePersonName() {
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
