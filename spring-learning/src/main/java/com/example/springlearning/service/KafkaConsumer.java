b806b2d64bd27634f4e1afa7c07c78b1e3579562
zhouruxuan <zhouruxuan@meituan.com>
2024-02-19 11:36:52 +0800

    补充了kafka 创建topic，获取通知结果的controller方法

--- spring-learning/src/main/java/com/example/springlearning/service/KafkaConsumer.java
+++ spring-learning/src/main/java/com/example/springlearning/service/KafkaConsumer.java
@@ -1,15 +1,16 @@
 package com.example.springlearning.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaConsumer {
 
     @KafkaListener(topics = "my-topic", groupId = "my-group-id")
     public void consume(String message) {
-        System.out.println("Received message: " + message);
+        LOGGER.info("Received message : {}", message);
     }
-
 }
 
