c9fa160b5c7c233bf933a360856219fe9df9317a
zhouruxuan <zhouruxuan@meituan.com>
2024-02-28 17:36:19 +0800

    把json测试移出来，没必要放在单测里

---
+++ java-learning/src/main/java/entity/JsonTestModel.java
@@ -0,0 +1,25 @@
+package entity;
+
+public class JsonTestModel {
+    private boolean vPoiInPromiseBlackList;
+
+    public boolean isvPoiInPromiseBlackList() {
+        return vPoiInPromiseBlackList;
+    }
+
+    public boolean isVPoiInPromiseBlackList() {
+        return vPoiInPromiseBlackList;
+    }
+
+    public void setVPoiInPromiseBlackList(boolean vPoiInPromiseBlackList) {
+        this.vPoiInPromiseBlackList = vPoiInPromiseBlackList;
+    }
+
+    public void setvPoiInPromiseBlackList(boolean vPoiInPromiseBlackList) {
+        this.vPoiInPromiseBlackList = vPoiInPromiseBlackList;
+    }
+
+    public void setVpoiInPromiseBlackList(boolean vPoiInPromiseBlackList) {
+        this.vPoiInPromiseBlackList = vPoiInPromiseBlackList;
+    }
+}
--- spring-learning/src/test/java/com/example/springlearning/entity/idl/TModel.thrift
+++
@@ -1,4 +0,0 @@
-struct com.example.springlearning.entity.thrift.TModel {
-  1: required bool mBoxId,
-  2: required bool mBoxId2,
-}
