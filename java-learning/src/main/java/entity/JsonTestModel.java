e661e53a1a1815f6efe7d8dc43d5e45c86d2a24d
zhouruxuan <zhouruxuan@meituan.com>
2024-03-04 17:38:18 +0800

    调整JsonTestModel的属性

--- java-learning/src/main/java/entity/JsonTestModel.java
+++ java-learning/src/main/java/entity/JsonTestModel.java
@@ -1,25 +1,63 @@
 package entity;
 
 public class JsonTestModel {
-    private boolean vPoiInPromiseBlackList;
+    private boolean vPoiIn1;
 
-    public boolean isvPoiInPromiseBlackList() {
-        return vPoiInPromiseBlackList;
+    private int vPoiIn2;
+
+    private boolean VPoiIn3;
+
+    private int VPoiIn4;
+
+    private boolean VpoiIn5;
+
+    private int VpoiIn6;
+
+    public boolean isvPoiIn1() {
+        return vPoiIn1;
     }
 
-    public boolean isVPoiInPromiseBlackList() {
-        return vPoiInPromiseBlackList;
+    public void setvPoiIn1(boolean vPoiIn1) {
+        this.vPoiIn1 = vPoiIn1;
     }
 
-    public void setVPoiInPromiseBlackList(boolean vPoiInPromiseBlackList) {
-        this.vPoiInPromiseBlackList = vPoiInPromiseBlackList;
+    public int getvPoiIn2() {
+        return vPoiIn2;
     }
 
-    public void setvPoiInPromiseBlackList(boolean vPoiInPromiseBlackList) {
-        this.vPoiInPromiseBlackList = vPoiInPromiseBlackList;
+    public void setvPoiIn2(int vPoiIn2) {
+        this.vPoiIn2 = vPoiIn2;
     }
 
-    public void setVpoiInPromiseBlackList(boolean vPoiInPromiseBlackList) {
-        this.vPoiInPromiseBlackList = vPoiInPromiseBlackList;
+    public boolean isVPoiIn3() {
+        return VPoiIn3;
+    }
+
+    public void setVPoiIn3(boolean VPoiIn3) {
+        this.VPoiIn3 = VPoiIn3;
+    }
+
+    public int getVPoiIn4() {
+        return VPoiIn4;
+    }
+
+    public void setVPoiIn4(int VPoiIn4) {
+        this.VPoiIn4 = VPoiIn4;
+    }
+
+    public boolean isVpoiIn5() {
+        return VpoiIn5;
+    }
+
+    public void setVpoiIn5(boolean vpoiIn5) {
+        VpoiIn5 = vpoiIn5;
+    }
+
+    public int getVpoiIn6() {
+        return VpoiIn6;
+    }
+
+    public void setVpoiIn6(int vpoiIn6) {
+        VpoiIn6 = vpoiIn6;
     }
 }
