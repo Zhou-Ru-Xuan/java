31684fb323914d696723bf7e0f420116f42c152b
zhouruxuan <zhouruxuan@meituan.com>
2024-03-22 11:04:32 +0800

    按位或的逆运算

--- java-learning/src/main/java/com/zhouruxuan/grammer/BitOperationTest.java
+++ java-learning/src/main/java/com/zhouruxuan/grammer/BitOperationTest.java
@@ -11,5 +11,18 @@ public class BitOperationTest {
         int result = i | j;
         result &= ~i;
         Assertions.assertEquals(result, j);
+
+        Assertions.assertEquals(40,reverseOr(40, 16));
+        Assertions.assertEquals(32,reverseOr(40, 24));
+        Assertions.assertEquals(40,reverseOr(40, 3));
+        Assertions.assertEquals(40,reverseOr(40, 6));
+
+    }
+
+    public int reverseOr(int sum, Integer... nums) {
+        for (Integer num : nums) {
+            sum &= ~num;
+        }
+        return sum;
     }
 }
