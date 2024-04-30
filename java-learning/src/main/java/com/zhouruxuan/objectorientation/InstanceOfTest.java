b4ef28d47593f7788bda169ae0e908f9dd7eb267
zhouruxuan <zhouruxuan@meituan.com>
2024-04-30 17:30:29 +0800

    testInstanceOfString

--- java-learning/src/main/java/com/zhouruxuan/objectorientation/InstanceOfTest.java
+++ java-learning/src/main/java/com/zhouruxuan/objectorientation/InstanceOfTest.java
@@ -1,8 +1,22 @@
 package com.zhouruxuan.objectorientation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InstanceOfTest {
+
+    @Test
+    public void testInstanceOfString() {
+        String s = "1";
+        method1(s);
+    }
+
+    private void method1(Object o) {
+        if (o instanceof String) {
+            Assertions.assertEquals("1", o);
+        }
+    }
+
     @Test
     public void testInstanceOf() {
         A a = new A();
@@ -21,6 +35,6 @@ class B extends A {
 
 }
 
-class C extends A{
+class C extends A {
 
 }
