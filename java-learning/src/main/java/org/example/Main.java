65d09bcdc8849d0cc6bc769c0356aee3f6491d45
zhouruxuan <zhouruxuan@meituan.com>
2024-06-06 15:06:52 +0800

    test conflict

--- java-learning/src/main/java/org/example/Main.java
+++ java-learning/src/main/java/org/example/Main.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Test");
 //        String json = "123";
 //        String json2 = "Invalid_Json";
 //        String json3 = "{}";
