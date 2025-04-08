8e7e950ce7b02b68e5d9e5bfe248bfc47dd19c0e
zhouruxuan <zhouruxuan@meituan.com>
2025-04-08 14:40:21 +0800

    iterator_test

--- java-learning/src/main/java/com/zhouruxuan/colletion/CollectionTest.java
+++ java-learning/src/main/java/com/zhouruxuan/colletion/CollectionTest.java
@@ -1,10 +1,22 @@
 package com.zhouruxuan.colletion;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class CollectionTest {
+    @Test
+    public void iterator_test(){
+        Set set = null;
+        Object firstResult = Optional.ofNullable(set).map(Set::iterator).map(Iterator::next).orElse(null);
+        Assert.assertNull(firstResult);
+
+        Set set2 = new HashSet();
+        // 校验异常
+        Exception exception = Assert.assertThrows(NoSuchElementException.class, () -> Optional.ofNullable(set2).map(Set::iterator).map(Iterator::next).orElse(null));
+        Assert.assertNotNull(exception);
+    }
     @Test
     public void testSize(){
         ArrayList arrayList = new ArrayList(10);
